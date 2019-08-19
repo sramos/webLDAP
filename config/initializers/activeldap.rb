a3dadb30b6b01b5734c4a5b4f00a3f42c24e735f
Santiago Ramos <sramos@sitiodistinto.net>
2019-08-19 18:12:28 +0200

    ActiveLdap connect

--- config/initializers/activeldap.rb
+++ config/initializers/activeldap.rb
@@ -1,14 +1,14 @@
 require 'active_ldap'
 ldap_config = YAML.load(ERB.new(File.read("#{Rails.root}/config/ldap.yml")).result(binding))[Rails.env]
-if ldap_config['host']
-  ActiveLdap::Base.setup_connection(
-    host: ldap_config['host'],
-    port: ldap_config['port'],
-    base: ldap_config['base'],
-    #:logger => logger_object,
-    bind_dn: ldap_config['admin_user'],
-    password_block: Proc.new { ldap_config['admin_password'] },
-    allow_anonymous: false,
-    try_sasl: false
-  )
+
+if ldap_config['host'] && ldap_config['base']
+  options = { host: ldap_config['host'], base: ldap_config[:base],
+              allow_anonymous: false, try_sasl: false }
+end
+
+if options 
+  options[:port] = ldap_config['port'] if ldap_config['port']
+  options[:bind_dn] = ldap_config['admin_user'] if ldap_config['admin_user']
+  options[:password_block] = Proc.new { ldap_config['admin_password'] } if ldap_config['admin_password']
+  ActiveLdap::Base.setup_connection(options)
 end
