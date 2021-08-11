ae886d8f6d5fc28005d4059ce167e2a51785bd4b
Santiago Ramos <sramos@sitiodistinto.net>
2021-08-11 13:20:37 +0200

    #1 Upgrade to rails 6.1

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
     resources :people
   end
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
