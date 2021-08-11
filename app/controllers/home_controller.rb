51779bcee5428f4bf1bab1fb2ffe45886536dc60
Santiago Ramos <sramos@sitiodistinto.net>
2021-08-11 11:50:33 +0200

    #2 Redirection after password change

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,25 @@
 class HomeController < ApplicationController
   def index
+    get_resources
+  end
+
+  def edit_password
+    @current_page = t('actions.password-title')
+  end
+
+  def update_password
+    current_user.update_password params
+    puts current_user.errors.inspect unless current_user.errors.empty?
+    if current_user.errors.blank?
+      redirect_to action: :index
+    else
+      render :edit_password
+    end
+  end
+
+  private
+
+  def get_resources
     @resources = [ {name: t('actions.password-title'),
                     icon: 'key', url: change_password_path},
                    {name: t('aliases.actions.fast-alias-title'),
@@ -10,15 +30,5 @@ class HomeController < ApplicationController
                    {name: 'URL shortner', icon: 'cut', url: 'http://crta.me/'},
                    {name: 'GitLab',       icon: 'code-branch', url: 'https://gitlab.com/sanraro'},
                    {name: 'Github',       icon: 'code-branch', url: 'https://github.com/sramos'} ]
-  end
-
-  def edit_password
-    @current_page = t('actions.password-title')
-  end
-
-  def update_password
-    current_user.update_password params
-    puts current_user.errors.inspect
-    render (current_user.errors.blank? ? :index : :edit_password)
   end
 end
